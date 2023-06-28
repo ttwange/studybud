10e9710bbbe24a35438b6a6ba92eece1d4564b2e
ttwange <ttwange2@gmail.com>
2023-06-28 22:48:36 +0300

    Finished on user registration

--- djangoToDo/ToDo/base/views.py
+++ djangoToDo/ToDo/base/views.py
@@ -1,6 +1,6 @@
 from typing import Any
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
@@ -29,6 +29,18 @@ class RegisterPage(FormView):
     form_class = UserCreationForm
     redirect_authenticated_user = True
     success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user =form.save()
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+    
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirected('tasks')
+        return super(RegisterPage, self).get(*args,**kwargs)
+    
 
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
