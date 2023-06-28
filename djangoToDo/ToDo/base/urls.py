db62d4da47bf80b48202a7cb3435e98b9af90ad3
ttwange <ttwange2@gmail.com>
2023-06-28 21:03:35 +0300

    Working on login page

--- djangoToDo/ToDo/base/urls.py
+++ djangoToDo/ToDo/base/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView
 
 urlpatterns = [
-    path('login/',CustomLoginView, name='login'),
+    path('login/',CustomLoginView.as_view(), name='login'),
     path('', TaskList.as_view(),name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(),name='task'),
     path('task-create/', TaskCreate.as_view(),name='task-create'),
