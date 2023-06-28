1356c1a7b71571e9de1366db5be0b49a376c74f5
ttwange <ttwange2@gmail.com>
2023-06-28 20:56:40 +0300

    Working on login page

--- djangoToDo/ToDo/base/views.py
+++ djangoToDo/ToDo/base/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Task
+from django.contrib.auth.views import LoginView
 
 # Create your views here.
+
+
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user=True
+
+    def get_success_url(self):
+        return reverse_lazy('task')
+
+
+
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
@@ -13,3 +29,18 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
