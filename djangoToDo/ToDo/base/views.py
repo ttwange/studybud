4d010093767b0be2e2617998042ea116f3e4fead
ttwange <ttwange2@gmail.com>
2023-06-29 20:00:05 +0300

    Working on the frontend page of the project

--- djangoToDo/ToDo/base/views.py
+++ djangoToDo/ToDo/base/views.py
@@ -1,25 +1,31 @@
-from typing import Any
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
-from .models import Task
+
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+# Imports for Reordering Feature
+from django.views import View
+from django.shortcuts import redirect
+from django.db import transaction
+
+from .models import Task
+from .forms import PositionForm
 
 
-# Create your views here.
 class CustomLoginView(LoginView):
     template_name = 'base/login.html'
     fields = '__all__'
-    redirect_authenticated_user=True
+    redirect_authenticated_user = True
 
     def get_success_url(self):
         return reverse_lazy('tasks')
+
 
 class RegisterPage(FormView):
     template_name = 'base/register.html'
@@ -28,16 +34,16 @@ class RegisterPage(FormView):
     success_url = reverse_lazy('tasks')
 
     def form_valid(self, form):
-        user =form.save()
+        user = form.save()
         if user is not None:
             login(self.request, user)
-        return super().form_valid(form)
-    
+        return super(RegisterPage, self).form_valid(form)
+
     def get(self, *args, **kwargs):
         if self.request.user.is_authenticated:
-            return redirected('tasks')
-        return super(RegisterPage, self).get(*args,**kwargs)
-    
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
 
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
@@ -50,30 +56,52 @@ class TaskList(LoginRequiredMixin, ListView):
 
         search_input = self.request.GET.get('search-area') or ''
         if search_input:
-            context['tasks'] = context['tasks'].filter(title__icontains=search_input)
+            context['tasks'] = context['tasks'].filter(
+                title__contains=search_input)
+
         context['search_input'] = search_input
+
         return context
 
-class TaskDetail(LoginRequiredMixin,DetailView):
+
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
 
-class TaskCreate(LoginRequiredMixin,CreateView):
+
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = ['title','description','complete']
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
     def form_valid(self, form):
         form.instance.user = self.request.user
-        return super(TaskCreate,self).form_valid(form)
+        return super(TaskCreate, self).form_valid(form)
 
-class TaskUpdate(LoginRequiredMixin,UpdateView):
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = ['title','description','complete']
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
-class DeleteView(LoginRequiredMixin,DeleteView):
+
+class DeleteView(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
+    def get_queryset(self):
+        owner = self.request.user
+        return self.model.objects.filter(user=owner)
+
+class TaskReorder(View):
+    def post(self, request):
+        form = PositionForm(request.POST)
+
+        if form.is_valid():
+            positionList = form.cleaned_data["position"].split(',')
+
+            with transaction.atomic():
+                self.request.user.set_task_order(positionList)
+
+        return redirect(reverse_lazy('tasks'))
