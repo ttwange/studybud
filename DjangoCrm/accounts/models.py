00a9f1aeafef6c4c1d3c3572ccf1c3ad1a27b68a
ttwange <ttwange2@gmail.com>
2023-07-03 09:22:15 +0300

    Working on Order tables

--- DjangoCrm/accounts/models.py
+++ DjangoCrm/accounts/models.py
@@ -9,3 +9,12 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+class Order
