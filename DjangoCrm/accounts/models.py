2a865777c009da7355a61be822e206beae7e613a
ttwange <ttwange2@gmail.com>
2023-07-04 00:29:33 +0300

    modified accounts

--- DjangoCrm/accounts/models.py
+++ DjangoCrm/accounts/models.py
@@ -22,9 +22,12 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     tag = models.ManyToManyField(Tag)
+    
+    def __str__(self):
+        return self.name
     
 class Order(models.Model):
     STATUS = (
@@ -36,4 +39,3 @@ class Order(models.Model):
     product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
-    
