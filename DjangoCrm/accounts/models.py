5bcd3fd4a72ccfc4a4e8d1e4beeef684190b7054
ttwange <ttwange2@gmail.com>
2023-07-03 23:38:50 +0300

    correcting a small bug on models file

--- DjangoCrm/accounts/models.py
+++ DjangoCrm/accounts/models.py
@@ -9,6 +9,10 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    def __str__(self):
+        return self.name
     
 class Product(models.Model):
     CATEGORY = (
@@ -20,11 +24,7 @@ class Product(models.Model):
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
     description = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
-
-class Tag(models.Model):
-    name = models.CharField(max_length=200, null=True)
-    def __str__(self):
-        return self.name
+    tag = models.ManyToManyField(Tag)
     
 class Order(models.Model):
     STATUS = (
@@ -36,4 +36,4 @@ class Order(models.Model):
     product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
-    tag = models.ManyToManyField(Tag)
+    
