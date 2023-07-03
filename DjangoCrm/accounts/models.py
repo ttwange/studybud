67483f7c51b94810d790deb039a1004d828acedc
ttwange <ttwange2@gmail.com>
2023-07-03 09:02:48 +0300

    Working on models file

--- DjangoCrm/accounts/models.py
+++ DjangoCrm/accounts/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Customer(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
     
