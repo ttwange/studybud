e34a59537183fa0909195643d5b7924a3560aa22
ttwange <ttwange2@gmail.com>
2023-07-04 08:05:44 +0300

    Working on rendering data to template

--- DjangoCrm/accounts/views.py
+++ DjangoCrm/accounts/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    context = {'orders':orders, 'customers':customers}
+    return render(request, 'accounts/dashboard.html', context)
+
 def customer(request):
     return render(request, 'accounts/customer.html')
+
 def products(request):
     products = Product.objects.all()
     return render(request, 'accounts/products.html', {'products':products})
