7cc0ae066a5ead84fee09dbc1ecb3c7730cea5e6
ttwange <ttwange2@gmail.com>
2023-07-04 21:21:22 +0300

    working on the CRUD functionality of the site

--- DjangoCrm/accounts/views.py
+++ DjangoCrm/accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 from .forms import OrderForm
@@ -31,5 +31,12 @@ def customer(request, pk):
 
 def createOrder(request):
     form = OrderForm()
+    if request.method == 'POST':
+        #print('Printing POST:', request.POST)
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
     context = {'form':form}
     return render(request, 'accounts/order_form.html', context)
