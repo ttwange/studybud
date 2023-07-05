eeb722e0f3209c9ea92cdca95480f167fc8c2665
ttwange <ttwange2@gmail.com>
2023-07-05 07:10:43 +0300

    Working on the inline formsets

--- DjangoCrm/accounts/views.py
+++ DjangoCrm/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 from .models import *
 from .forms import OrderForm
 
@@ -30,15 +31,17 @@ def customer(request, pk):
     return render(request, 'accounts/customer.html', context)
 
 def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('products','status'))
     customer = Customer.objects.get(id=pk)
-    form = OrderForm(initial={'customer':customer})
+    #form = OrderForm(initial={'customer':customer})
+    formset = OrderFormSet(instance=customer)
     if request.method == 'POST':
         #print('Printing POST:', request.POST)
         form = OrderForm(request.POST)
         if form.is_valid():
             form.save()
         return redirect('/')
-    context = {'form':form}
+    context = {'formset':formset}
     return render(request, 'accounts/order_form.html', context)
 
 
