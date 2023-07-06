81cf6cc7194c06ef2dc72d76ff2a435651ed6ee4
ttwange <ttwange2@gmail.com>
2023-07-06 06:05:47 +0300

    Working on the search functionality of the project

--- DjangoCrm/accounts/filters.py
+++ DjangoCrm/accounts/filters.py
@@ -1,8 +1,10 @@
 import django_filters
 from .models import *
+from django_filters import DateFilter
 
 
 class OrderFilter(django_filters.FilterSet):
     class Meta:
         model = Order
         fields = '__all__'
+        exclude =['customer','date_created']
