b6f000700c3d99aa028fc49c40087ab246edbc33
ttwange <ttwange2@gmail.com>
2023-07-06 08:18:07 +0300

    Login page bug fix

--- DjangoCrm/accounts/views.py
+++ DjangoCrm/accounts/views.py
@@ -37,7 +37,12 @@ def registerPage(request):
 
 def loginPage(request):
     if request.method == 'POST':
-        request.POST
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
     context={}
     return render(request, 'accounts/login.html', context)
 
