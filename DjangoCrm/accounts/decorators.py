908792cf03357004457aa8d9bdf5ef982c9de806
ttwange <ttwange2@gmail.com>
2023-07-07 16:30:50 +0300

    Trying restriction functionality

--- DjangoCrm/accounts/decorators.py
+++ DjangoCrm/accounts/decorators.py
@@ -12,7 +12,12 @@ def unauthenticated_user(view_func):
 def allowed_users(allowed_roles=[]):
     def decorators(view_func):
       def wrapper_func(request, *args, **kwargs):
-          print("Working", allowed_roles)
-          return view_func(request, *args, **kwargs)
+          group = None
+          if request.users.groups.exist():
+              group = request.groups.all()[0].name
+          if group in allowed_roles:
+              return view_func(request, *args, **kwargs)
+          else:
+              return HttpResponse("You are Not authorized to Access this page") 
       return wrapper_func
     return decorators
