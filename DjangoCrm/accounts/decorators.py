c5290323ef1877b28184c632c45fc2f57f3efcea
ttwange <ttwange2@gmail.com>
2023-07-07 16:37:25 +0300

    Bug fix on decorators

--- DjangoCrm/accounts/decorators.py
+++ DjangoCrm/accounts/decorators.py
@@ -10,14 +10,14 @@ def unauthenticated_user(view_func):
     return  wrapper_function
 
 def allowed_users(allowed_roles=[]):
-    def decorators(view_func):
+    def decorator(view_func):
       def wrapper_func(request, *args, **kwargs):
           group = None
-          if request.users.groups.exist():
-              group = request.groups.all()[0].name
+          if request.user.groups.exists():
+              group = request.user.groups.all()[0].name
           if group in allowed_roles:
               return view_func(request, *args, **kwargs)
           else:
               return HttpResponse("You are Not authorized to Access this page") 
       return wrapper_func
-    return decorators
+    return decorator
