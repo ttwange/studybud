36f06b5632999be0aee14632108e4452ab4c9850
ttwange <ttwange2@gmail.com>
2023-07-07 16:53:38 +0300

    Complete a rol permission based function

--- DjangoCrm/accounts/decorators.py
+++ DjangoCrm/accounts/decorators.py
@@ -15,7 +15,9 @@ def allowed_users(allowed_roles=[]):
           group = None
           if request.user.groups.exists():
               group = request.user.groups.all()[0].name
-          if group in allowed_roles:
+          if group == 'customer':
+              return redirect('user-page')
+          if group == 'admin':
               return view_func(request, *args, **kwargs)
           else:
               return HttpResponse("You are Not authorized to Access this page") 
