b4c50ded44b170bbc43d65551acfd2f7cbb03555
ttwange <ttwange2@gmail.com>
2023-07-08 10:13:14 +0300

    buidling the customer profile styling

--- DjangoCrm/accounts/views.py
+++ DjangoCrm/accounts/views.py
@@ -62,8 +62,10 @@ def home(request):
 @allowed_users(allowed_roles=['customer'])
 def userPage(request):
     orders = request.user.customer.order_set.all()
-    print('ORDERS:', orders)
-    context = {'orders':orders}
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    context = {'orders':orders,'total_orders':total_orders,'delivered':delivered,'pending':pending}
     return render(request, 'accounts/user.html', context)
 
 @login_required(login_url='login')
