8b67fb12d7cef9348600bd32be52e9f452830c12
ttwange <ttwange2@gmail.com>
2023-07-10 22:11:22 +0300

    Adding the password reset confirmation view

--- DjangoCrm/accounts/urls.py
+++ DjangoCrm/accounts/urls.py
@@ -19,5 +19,5 @@ urlpatterns = [
     path('reset_password/', auth_views.PasswordResetView.as_view()),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view()),
     path('reset_password/', auth_views.PasswordResetConfirmView.as_view()),
-    path('reset_password/', auth_views.PasswordResetView.as_view()),
+    path('reset_password/', auth_views.PasswordResetCompleteView.as_view()),
 ]
