eedc6e1a1cb73f472c15dd1b81fc2f49cc87e3ae
ttwange <ttwange2@gmail.com>
2023-07-10 13:22:24 +0300

    working on submission of files

--- DjangoCrm/accounts/views.py
+++ DjangoCrm/accounts/views.py
@@ -77,6 +77,10 @@ def userPage(request):
 def accountSettings(request):
     customer = request.user.customer
     form = CustomerForm(instance=customer)
+    if request.method == "POST":
+        form = CustomerForm(request.POST, request.FILES, instance=customer)
+        if form.is_valid():
+            form.save()
     context={'form':form}
     return render(request, 'accounts/account_settings.html', context)
 
