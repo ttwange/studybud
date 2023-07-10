93c16238e40c395b7b781e8ad4b19018a30def0f
ttwange <ttwange2@gmail.com>
2023-07-10 22:45:38 +0300

    django template bypass in reset paasword

--- DjangoCrm/accounts/urls.py
+++ DjangoCrm/accounts/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('update_Order/<str:pk>/', views.updateOrder, name="update_Order"),
     path('delete_Order/<str:pk>/', views.deleteOrder, name="delete_Order"),
 
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="accounts/"), name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
