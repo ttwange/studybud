0f18f463fe9f6e67972487967f9d23a062f0603b
ttwange <ttwange2@gmail.com>
2023-07-10 21:30:35 +0300

    Creating a signals file to handle some of the functionality

--- DjangoCrm/accounts/signals.py
+++ DjangoCrm/accounts/signals.py
@@ -1,3 +1,14 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from .models import Customer
+
+
+def customer_profile(sender, instance, created, **kwargs):
+    if created:
+        group = Group.objects.get(name='customer')
+        instance.groups.add(group)
+        Customer.objects.create(
+            user = instance,
+            )
+
+        
--- DjangoCrm/accounts/views.py
+++ DjangoCrm/accounts/views.py
@@ -18,12 +18,6 @@ def registerPage(request):
         form =CreateUserForm(request.POST)
         if form.is_valid():
             user = form.save()
-            group = Group.objects.get(name='customer')
-            user.groups.add(group)
-            Customer.objects.create(
-                user = user,
-                )
-
             messages.success(request, 'Account successful created')
             return redirect('login')
         
